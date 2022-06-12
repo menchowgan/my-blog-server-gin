bfab590718a69cdc1731cd7504ef58bdc260ac07
menchowgan <1657943220@qq.com>
2022-06-12 11:58:17 +0800

    modify source path

--- config/index.go
+++ config/index.go
@@ -7,9 +7,9 @@ const (
 	PASSWORD        = "menchowgan"
 	DATABASE_NAME   = "gmc_blog"
 
-	PHOTO_QUERY_PATH = "http://localhost/"
-	MUSCI_QUERY_PATH = "http://localhost/"
-	VIDEO_QUERY_PATH = "http://localhost/"
+	PHOTO_QUERY_PATH = "http://8.136.84.136/"
+	MUSCI_QUERY_PATH = "http://8.136.84.136/"
+	VIDEO_QUERY_PATH = "http://8.136.84.136/"
 	PHOTO_PATH       = "/usr/local/share/images/"
 	MUSCI_PATH       = "/usr/local/share/music/"
 	VIDEO_PATH       = "/usr/local/share/video/"
