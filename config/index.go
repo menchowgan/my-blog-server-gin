29f0acf75e02593757027e87d9986ef9699a2bd3
menchowgan <1657943220@qq.com>
2022-06-12 12:02:12 +0800

    path

--- config/index.go
+++ config/index.go
@@ -7,9 +7,9 @@ const (
 	PASSWORD        = "menchowgan"
 	DATABASE_NAME   = "gmc_blog"
 
-	PHOTO_QUERY_PATH = "http://8.136.84.136/"
-	MUSCI_QUERY_PATH = "http://8.136.84.136/"
-	VIDEO_QUERY_PATH = "http://8.136.84.136/"
+	PHOTO_QUERY_PATH = "http://8.136.84.136:81/"
+	MUSCI_QUERY_PATH = "http://8.136.84.136:81/"
+	VIDEO_QUERY_PATH = "http://8.136.84.136:81/"
 	PHOTO_PATH       = "/usr/local/share/images/"
 	MUSCI_PATH       = "/usr/local/share/music/"
 	VIDEO_PATH       = "/usr/local/share/video/"
