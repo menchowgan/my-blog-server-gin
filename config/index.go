b730be817f2b4282dcd152059dae786be2498a42
menchowgan <1657943220@qq.com>
2022-06-25 16:53:34 +0800

    localhost branch

--- config/index.go
+++ config/index.go
@@ -7,9 +7,9 @@ const (
 	PASSWORD        = "menchowgan"
 	DATABASE_NAME   = "gmc_blog"
 
-	PHOTO_QUERY_PATH = "http://8.136.84.136:81/"
-	MUSCI_QUERY_PATH = "http://8.136.84.136:81/"
-	VIDEO_QUERY_PATH = "http://8.136.84.136:81/"
+	PHOTO_QUERY_PATH = "http://localhost/"
+	MUSCI_QUERY_PATH = "http://localhost/"
+	VIDEO_QUERY_PATH = "http://localhost/"
 	PHOTO_PATH       = "/usr/local/share/images/"
 	MUSCI_PATH       = "/usr/local/share/music/"
 	VIDEO_PATH       = "/usr/local/share/video/"
